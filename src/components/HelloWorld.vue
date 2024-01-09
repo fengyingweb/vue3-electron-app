7ce3cf18e1c816dbff5cf5820f33a3f6e0ce042a
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-09 18:02:23 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>chrome{{versions.chrome}}，electron{{versions.electron}}，node{{versions.node}}</p>
   <div class="item">
-    <input type="text" v-model="inputVal">
+    <input class="title-input" type="text" v-model="inputVal">
     <button type="button" @click="clickSetTitle">设置标题</button>
   </div>
   <div class="item">
@@ -15,10 +15,14 @@
     <button type="button" @click="clickToggle">切换主题</button>
     <button type="button" @click="clickRest">重置主题</button>
   </div>
+  <el-icon size="20" color="#f56">
+    <Edit />
+  </el-icon>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+console.log(import.meta.env)
 defineProps({
   msg: String
 })
@@ -55,5 +59,12 @@ a {
 }
 .bold {
   font-weight: bold;
+}
+.title-input {
+  margin-right: 10px;
+  border: 1px solid #42b983;
+  padding: 0 5px;
+  line-height: 30px;
+  border-radius: 4px;
 }
 </style>
