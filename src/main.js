e42fd64ed70779d52e975a1b8078dad46baea2a8
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-09 18:01:46 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import './assets/css/style.css'
 import './assets/css/base.less'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.mount('#app')
