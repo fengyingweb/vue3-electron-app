318849bdcc755494f20c68ead581d0739f15feee
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-09 18:01:14 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,7 +13,33 @@ export default defineConfig({
     electron({
       entry: 'electron/main.js'
     }),
-    renderer()
+    renderer(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
-  base: './'
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+    },
+    extensions: ['.js', '.ts', '.vue', '.jsx', '.tsx', '.json']
+  },
+  base: './', // 设置打包后文件引用路径
+  server: {
+    port: 3002,
+    host: '0.0.0.0',
+    open: false,
+    cors: true, // 允许跨域
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:3001',
+        ws: true,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\//, 'api')
+      }
+    }
+  },
 })
