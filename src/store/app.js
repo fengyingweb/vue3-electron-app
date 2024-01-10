b762482432cf33a8145c2daf983e203503a4a8d3
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-10 17:36:48 +0800

    Create app.js

---
+++ src/store/app.js
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia'
+import { useCache } from '@/hooks/useChache.js'
+
+const { webCache } = useCache('localStorage')
+
+export default useAppStore = defineStore('app', {
+  state: ()=> {
+    return {
+      isCollapse: false,
+      breadCrumbList: webCache.get('breadCrumbList') ? webCache.get('breadCrumbList') : [],
+      isDarkMode: webCache.get('isDarkMode') ? webCache.get('isDarkMode') : false,
+    }
+  },
+  actions: {
+    // 设置主题切换
+    setDark(isDark){
+      this.isDarkMode = isDark
+      webCache.set('isDarkMode', this.isDarkMode)
+    },
+    // 设置菜单合并
+    setCollapse(bool) {
+      this.isCollapse = bool
+    },
+    // 设置面包屑路由展示
+    setBreadCrumbList(data, action){
+        if(action === 'route'){
+            const newData=[...this.breadCrumbList]
+            const filterData = newData.map((item)=>{
+                if(item.type !== 'route'){
+                    return {...item}
+                }
+            }).filter(item=>item !== undefined)
+            this.breadCrumbList=[data, ...filterData]
+        }else{
+            this.breadCrumbList.push(data)
+        }
+        webCache.set('breadCrumbList',this.breadCrumbList)
+    }
+  }
+})
