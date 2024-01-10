a83b7d3cf222b6b100c13c6994289fe43bb36541
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-10 17:30:21 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router/index.js'
 import App from './App.vue'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(ElementPlus)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
+app.use(router).use(pinia)
 app.mount('#app')
