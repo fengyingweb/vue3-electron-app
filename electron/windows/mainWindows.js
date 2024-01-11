094dfae50e447f4242d34614da6d6110ec90e9a0
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-11 18:19:44 +0800

    Update mainWindows.js

--- electron/windows/mainWindows.js
+++ electron/windows/mainWindows.js
@@ -5,7 +5,7 @@ const isDev = require('electron-is-dev')
 const windowStateKeeper = require('electron-window-state')
 const mainWinURL = isDev ? `http://localhost:3002/#/` : `${LOAD_URL}#`;
 
-const createMainWindow=(BrowserWindow)=>{
+const createMainWindow=(BrowserWindow, nativeTheme)=>{
     // 默认窗口尺寸
     let mainWindowState = windowStateKeeper({
         defaultWidth: 1000,
@@ -35,6 +35,7 @@ const createMainWindow=(BrowserWindow)=>{
     // 优雅打开界面
     win.on('ready-to-show',()=>{
         win.show()
+        win.webContents.send('on-get-theme', nativeTheme.shouldUseDarkColors)
     })
     global.mainWindow = win
 }
