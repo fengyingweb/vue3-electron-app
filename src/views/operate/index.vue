81c2881c5dabb100d63274b692d5b787342e789a
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-11 17:54:11 +0800

    Create index.vue

---
+++ src/views/operate/index.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="wraper-container">
+    {{title}}
+     <el-button type="success">Success</el-button>
+   </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import {useCache} from '@/hooks/useCache'
+const {wsCache}=useCache('localStorage')
+
+const title = ref('运营管理')
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
