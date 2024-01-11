dfbf272e19b3ca5b2795aaad1f834bacee414537
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-11 15:50:10 +0800

    Create AppMain.vue

---
+++ src/layout/AppMain.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-main-container">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppMain"
+}
+</script>
+
+<style lang="scss" scoped>
+.app-main-container{
+    width: 100%;
+  height: calc(100vh - #{$topHeaderHeight} - #{$navigationHeight});
+  background: #F5F6F7;
+}
+</style>
