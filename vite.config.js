bb3e312bbcaf6fd017f05a1b4307c3978d4c41cc
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-11 17:03:08 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,15 +17,32 @@ export default defineConfig({
     // }),
     // renderer(),
     AutoImport({
-      resolvers: [ElementPlusResolver({
-        importStyle:'sass'
-      })],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle:'sass'
+        }),
+        // Auto import icon components
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
     }),
     Components({
-      resolvers: [ElementPlusResolver({
-        importStyle:'sass'
-      })],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle:'sass'
+        }),
+        // Auto register icon components
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep'],
+        })
+      ],
     }),
+    Icons({
+      autoInstall: true,
+    })
   ],
   css: {
     preprocessorOptions: {
