c7160e76c1d498521f91df2fbae6815fa961c747
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-11 17:25:57 +0800

    Create index.vue

---
+++ src/views/review/index.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="wraper-container">
+    {{count}}
+   </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import {useCache} from '@/hooks/useCache'
+const {wsCache}=useCache('localStorage')
+
+const count = ref('审阅区')
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
