cdfa3d9944ffa561e87882ebc21a42ed2e10efe2
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-11 17:51:14 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -44,6 +44,99 @@ export const routes = [
         }
     ]
   },
+  {
+    path: '/reviewHistory',
+    name: 'ReviewHistory',
+    component: LayOut,
+    children:[
+        {
+            path: '/ReviewHistory',
+            name: 'ReviewHistory',
+            meta: {
+                title: '审阅区历史',
+                icon:imgLocalUrl('@/assets/layout/history.png'),
+            },
+            component: () => import('@/views/review-history/index.vue'),
+        },
+    ]
+  },
+  {
+      path: '/notice',
+      name: 'Notice',
+      component: LayOut,
+      children:[
+          {
+              path: '/notice',
+              name: 'Notice',
+              meta: {
+                  title: '消息提醒',
+                  icon:imgLocalUrl('@/assets/layout/xiaoxitixing.png'),
+              },
+              component: () => import('@/views/notice/index.vue'),
+          },
+      ]
+  },
+  {
+      path: '/settings',
+      name: 'Settings',
+      component: LayOut,
+      children:[
+          {
+              path: '/settings',
+              name: 'Settings',
+              meta: {
+                  title: '配置管理',
+                  icon:imgLocalUrl('@/assets/layout/settings.png'),
+              },
+              component: () => import('@/views/settings/index.vue'),
+          },
+      ]
+  },
+  {
+      path: '/user',
+      name: 'User',
+      component: LayOut,
+      children:[
+          {
+              path: '/user',
+              name: 'User',
+              meta: {
+                  title: '用户管理',
+                  icon:imgLocalUrl('@/assets/layout/user.png'),
+              },
+              component: () => import('@/views/user/index.vue'),
+          },
+      ]
+  },
+  {
+      path: '/operate',
+      name: 'Operate',
+      component: LayOut,
+      children:[
+          {
+              path: '/operate',
+              name: 'Operate',
+              meta: {
+                  title: '运营管理',
+                  icon:imgLocalUrl('@/assets/layout/operate.png'),
+              },
+              component: () => import('@/views/operate/index.vue'),
+          },
+      ]
+  },
+  {
+      path: '/news',
+      name: 'News',
+      component: LayOut,
+      hidden: true,
+      children:[
+          {
+              path: '/news',
+              name: 'News',
+              component: () => import('@/views/news/index.vue'),
+          },
+      ]
+  },
 ]
 
 const router = createRouter({
