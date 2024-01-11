672164be34fd3e553fe02d76485bbe729f03e32c
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-11 17:52:26 +0800

    Create index.vue

---
+++ src/views/notice/index.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="wraper-container">
+    {{title}}
+   </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import {useCache} from '@/hooks/useCache'
+const {wsCache}=useCache('localStorage')
+
+const title = ref('消息提醒')
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
