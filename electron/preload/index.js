d4bc4f6768f650d6df71e16ff318d82cc6722b2f
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-11 11:31:52 +0800

    Update index.js

--- electron/preload/index.js
+++ electron/preload/index.js
@@ -7,7 +7,9 @@ contextBridge.exposeInMainWorld('electronAPI', {
     node: process.versions.node,
   },
   setTitle: (title)=> ipcRenderer.send('set-title', title), // 渲染进程发送消息给主进程
-  openFile: ()=> ipcRenderer.invoke('dialog:openFile'), // 双向通信
-  toggleTheme: ()=> ipcRenderer.invoke('dark-mode:toggle'),
-  systemTheme: ()=> ipcRenderer.invoke('dark-mode:system')
+  openFile: ()=> ipcRenderer.invoke('dialog:openFile'),
+  openByBrowser: (url)=> ipcRenderer.invoke('on-useOpenByBrowser-event', url), // 双向通信 浏览器打开页面
+  toggleTheme: ()=> ipcRenderer.invoke('dark-mode:toggle'), // 切换界面主题
+  systemTheme: ()=> ipcRenderer.invoke('dark-mode:system'),
+  setScreen: (value)=> ipcRenderer.invoke('on-setScreen-event', value) // 页面全屏 缩小 关闭
 })
