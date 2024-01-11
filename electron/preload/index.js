39fc12d3be91142a5e70e609d0a774e09e78aaa7
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-11 14:06:41 +0800

    Update index.js

--- electron/preload/index.js
+++ electron/preload/index.js
@@ -11,5 +11,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
   openByBrowser: (url)=> ipcRenderer.invoke('on-useOpenByBrowser-event', url), // 双向通信 浏览器打开页面
   toggleTheme: ()=> ipcRenderer.invoke('dark-mode:toggle'), // 切换界面主题
   systemTheme: ()=> ipcRenderer.invoke('dark-mode:system'),
-  setScreen: (value)=> ipcRenderer.invoke('on-setScreen-event', value) // 页面全屏 缩小 关闭
+  setScreen: (value)=> ipcRenderer.invoke('on-setScreen-event', value), // 页面全屏 缩小 关闭
+  shortcut: (callback)=> ipcRenderer.on('on-shortcut-event', (_event, value)=> callback(value))
 })
