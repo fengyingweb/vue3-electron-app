57ccde20fa958ed744d3ab4f83a146bfa31e6b9b
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-11 17:51:55 +0800

    Create index.vue

---
+++ src/views/review-history/index.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="wraper-container">
+    {{title}}
+   </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import {useCache} from '@/hooks/useCache'
+const {wsCache}=useCache('localStorage')
+
+const title = ref('审阅区历史')
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
