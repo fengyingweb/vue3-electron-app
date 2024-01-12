a4f4b4ae6af15e58e91a5a7ee707f1638395775a
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-12 14:59:02 +0800

    Update index.vue

--- src/views/review/index.vue
+++ src/views/review/index.vue
@@ -1,19 +1,34 @@
 <template>
    <div class="wraper-container">
-    {{count}}
+    <div>
+       <span>{{title}}</span>
+       <el-button class="btn-item" type="danger" @click="goToNews">查看新闻</el-button>
+    </div>
    </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import {useCache} from '@/hooks/useCache'
-const {wsCache}=useCache('localStorage')
+import {useOpenByBrowser} from '@/hooks/useOpenByBrowser'
 
-const count = ref('审阅区')
+const {webCache} = useCache('localStorage')
+const {openByBrowser} = useOpenByBrowser()
 
+const title = ref('审阅区')
+
+const goToNews = async ()=> {
+   const res = await openByBrowser(`${location.origin}/#/news`)
+   console.log(res)
+}
 
 </script>
 
 <style lang="scss" scoped>
-
+.wraper-container {
+     padding: 14px;
+}
+.btn-item {
+     margin-left: 15px;
+}
 </style>
