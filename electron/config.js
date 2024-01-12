824387d8888c8423f270ebc06aa3bc47a97c53a5
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-12 14:59:23 +0800

    Update config.js

--- electron/config.js
+++ electron/config.js
@@ -1,6 +1,6 @@
 const ACHEME = 'file';
 const path=require('node:path')
-const LOAD_URL=`${ACHEME}://${path.join(__dirname, '../dist/index.html')}`
+const LOAD_URL=`${ACHEME}://${path.join(__dirname, '../dist/index.html/')}`
 module.exports={
     ACHEME,
     LOAD_URL
