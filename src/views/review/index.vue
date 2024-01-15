b20bbb53d4ae641564b3457d694840fd068b7bde
fengyingweb <31813362+fengyingweb@users.noreply.github.com>
2024-01-15 16:40:09 +0800

    Update index.vue

--- src/views/review/index.vue
+++ src/views/review/index.vue
@@ -1,8 +1,12 @@
 <template>
    <div class="wraper-container">
-    <div>
+    <div class="item">
        <span>{{title}}</span>
        <el-button class="btn-item" type="primary" @click="goToNews">查看新闻</el-button>
+    </div>
+    <div class="item">
+       <span class="bold">文件路径：{{filePath}}</span>
+       <el-button class="btn-item" type="primary" @click="handleOpenFile">打开文件</el-button>
     </div>
    </div>
 </template>
@@ -16,10 +20,16 @@ const {webCache} = useCache('localStorage')
 const {openByBrowser} = useOpenByBrowser()
 
 const title = ref('审阅区')
+const filePath = ref('')
 
 const goToNews = async ()=> {
    const res = await openByBrowser(`${location.origin}/#/news`)
    console.log(res)
+}
+
+const handleOpenFile = async ()=> {
+   const resPath = await window.electronAPI.openFile()
+   filePath.value = resPath
 }
 
 </script>
@@ -28,7 +38,13 @@ const goToNews = async ()=> {
 .wraper-container {
      padding: 14px;
 }
+.item {
+    margin-bottom: 10px;
+}
 .btn-item {
      margin-left: 15px;
+}
+.bold {
+    font-weight: bold;
 }
 </style>
